63e93858f7347ef074fc4054f8ba01f7d4475bbd
guangxin <401040990@qq.com>
2024-12-23 01:41:07 +0800

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  base: '/your-repo-name/',
+  base: '/hk-guide/',
   plugins: [vue()],
   resolve: {
     alias: {

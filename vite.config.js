13d92ed2fa9cd58c6ed4b99e2ed9d1c3c256cafd
guangxin <401040990@qq.com>
2024-12-23 08:56:01 +0800

    fix code

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
 export default defineConfig({
-  base: '/hk-guide/',
+  base: process.env.NODE_ENV === 'production' ? '/hk-guide/' : '/',
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  server: {
-    port: 3000,
-    open: true
-  }
 }) 
--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,5 @@
- 
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/hk-guide/'  // 替换成你的仓库名
+    : '/'
+} 

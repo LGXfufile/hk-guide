a95a0e85596fd9648546e9de9a1a27307d741146
guangxin <401040990@qq.com>
2024-12-23 22:49:58 +0800

    fix code

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
 const pinia = createPinia()
@@ -14,8 +14,8 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
-app.use(ElementPlus)
 app.use(router)
 app.use(pinia)
+app.use(ElementPlus)
 
 app.mount('#app') 
